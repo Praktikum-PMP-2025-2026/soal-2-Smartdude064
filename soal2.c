3a27e46546690b9c19d52d7f54f3453ccc69877b
Faiz Azmi <faizazmi.vasta21@gmail.com>
2026-04-15 09:05:52 +0700

    #1 Code Test
    submit kode versi pertama

--- soal2.c
+++ soal2.c
@@ -1,5 +1,49 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void) {
-    return 0;
+// fungsi rekursi yang berguna dalam melakukan looping transformasi (algoritma utama)
+int recursion(int j, int n) {
+    if (n % 2 == 0) {
+        n /= 2;
+        } else if (n % 2 != 0) {
+            n = 3*n + 1;
+        }
+        //printf("Iterasi ke-%d : %d\n", j+1, n);
+        j++;
+        if (n == 1) {
+            return j;
+        }
+    return recursion(j, n);
+}
+
+// fungsi utama program ketika berjalan
+int main() {
+    // deklarasi variabel
+    int n, T, j, arr[100];
+
+    //input dari user
+    scanf("%d", &T);
+    for (int i = 0; i < T; i++) {
+        scanf("%d", &n);
+        arr[i] = n;
+    }
+
+    //looping dalam menghasilkan output sebanyak T 
+    for (int i = 0; i < T; i++) {
+        /*while (n != 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else if (n % 2 != 0) {
+                n = 3*n + 1;
+            } else if (n == 1) {
+                break;
+            }
+            printf("Iterasi ke-%d : %d\n", j+1, n);
+            j++;
+        }*/
+        
+        //output ke terminal
+        j = recursion(0, arr[i]);
+        printf("LANGKAH %d\n", j);
+    }
 }
